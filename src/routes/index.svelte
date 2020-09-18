f6829b5d4fc7d58fc6e145a6a9559395dd7fae94
jaime <garciadiazjaime@gmail.com>
2020-09-17 20:00:48 -0500

    feat: init

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+  import { getPlaces } from "../utils/mintAPI";
+
+  let places = [];
+
+  onMount(async () => {
+    places = await getPlaces();
+	});
+</script>
+
+<svelte:head>
+	<title>MigriMap</title>
+</svelte:head>
+
+
+{#if places}
+	{#each places as place}
+		{place.name} 
+	{/each}
+{/if}
---
+++ src/utils/mintAPI.js
@@ -0,0 +1,58 @@
+function getPlacesQuery(id = '' ) {
+  return `
+    {
+      migriPlace(id: "${id}") {
+        _id
+        name
+        description
+        profile
+        address
+        gmaps
+        phone
+        servicesFree
+        servicesNonFree
+        img
+        website
+        socialNetwork
+        ceo
+        owner
+        language
+        schedule
+        capacity
+        population
+        category
+      }
+    }
+  `
+}
+
+async function requestHelper(payload) {
+  const result = await fetch(
+    `process.API_URL/graphiql`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    }
+  );
+
+  return await result.json()
+}
+
+async function getPlaces(id) {
+  const payload = {
+    query: getPlacesQuery(id)
+  };
+
+  const {
+    data: { migriPlace }
+  } = await requestHelper(payload)
+
+  return migriPlace;
+}
+
+export {
+  getPlaces,
+} 
